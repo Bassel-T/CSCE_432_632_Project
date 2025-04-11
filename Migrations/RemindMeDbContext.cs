231a660684df9484f013ff8751264b88b8ea1be2
Bassel T <lessab99@gmail.com>
2025-04-10 20:48:56 -0500

    Ticket #4 : Generated User Model into the database

--- Migrations/RemindMeDbContext.cs
+++ Migrations/RemindMeDbContext.cs
@@ -1,15 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CSCE_432_632_Project.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CSCE_432_632_Project.Migrations
 {
     public class RemindMeDbContext : DbContext
     {
         // Tables here
+        public DbSet<User> Users { get; set; }
 
-
-        public RemindMeDbContext(DbContextOptions<RemindMeDbContext> options) : base(options)
+        public RemindMeDbContext(DbContextOptions<RemindMeDbContext> options, ILoggerFactory loggerFactory) : base(options)
         {
-
+            
         }
     }
 }
