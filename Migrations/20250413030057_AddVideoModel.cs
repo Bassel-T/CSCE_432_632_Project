23567444a797bbc10c8c432e42c6a8674f99e078
Bassel T <lessab99@gmail.com>
2025-04-12 22:09:26 -0500

    Ticket #7 : Created the Video model

---
+++ Migrations/20250413030057_AddVideoModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CSCE_432_632_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVideoModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Videos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RoomId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ScheduledDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    CreatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Videos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Videos_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Videos_RoomId",
+                table: "Videos",
+                column: "RoomId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Videos");
+        }
+    }
+}
--- Migrations/RemindMeDbContext.cs
+++ Migrations/RemindMeDbContext.cs
@@ -9,6 +9,7 @@ namespace CSCE_432_632_Project.Migrations
         public DbSet<User> Users { get; set; }
         public DbSet<Room> Rooms { get; set; }
         public DbSet<UserRoom> UserRooms { get; set; }
+        public DbSet<Video> Videos { get; set; }
 
         public RemindMeDbContext(DbContextOptions<RemindMeDbContext> options, ILoggerFactory loggerFactory) : base(options)
         {
@@ -24,6 +25,9 @@ namespace CSCE_432_632_Project.Migrations
             modelBuilder.Entity<UserRoom>().HasOne(ur => ur.User);
             modelBuilder.Entity<UserRoom>().HasOne(ur => ur.Room);
             modelBuilder.Entity<UserRoom>().Property(x => x.Role).HasConversion<string>();
+
+            // Configure the Video table
+            modelBuilder.Entity<Video>().HasOne(v => v.Room);
         }
     }
 }
