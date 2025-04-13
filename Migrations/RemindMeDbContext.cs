bd04b43a6cbc20e98f3c48bd11c7090cc7ce4299
Bassel T <lessab99@gmail.com>
2025-04-12 21:47:43 -0500

    Ticket #6 : Created the UserRoom model

--- Migrations/RemindMeDbContext.cs
+++ Migrations/RemindMeDbContext.cs
@@ -8,10 +8,22 @@ namespace CSCE_432_632_Project.Migrations
         // Tables here
         public DbSet<User> Users { get; set; }
         public DbSet<Room> Rooms { get; set; }
+        public DbSet<UserRoom> UserRooms { get; set; }
 
         public RemindMeDbContext(DbContextOptions<RemindMeDbContext> options, ILoggerFactory loggerFactory) : base(options)
         {
             
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure the UserRoom table
+            modelBuilder.Entity<UserRoom>().HasKey(ur => new { ur.UserId, ur.RoomId });
+            modelBuilder.Entity<UserRoom>().HasOne(ur => ur.User);
+            modelBuilder.Entity<UserRoom>().HasOne(ur => ur.Room);
+            modelBuilder.Entity<UserRoom>().Property(x => x.Role).HasConversion<string>();
         }
     }
 }
